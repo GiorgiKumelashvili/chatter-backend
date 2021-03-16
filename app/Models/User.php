d88d9026951a291aef845b5415ebb7f8a3b75581
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2021-03-16 23:03:13 +0400

    little refactor

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,21 +9,25 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property mixed id
+ * @property mixed email
+ */
 class User extends Authenticatable implements JwtPayloadInterface {
     use HasFactory, Notifiable;
 
-    protected array $fillable = [
+    protected $fillable = [
         'id',
         'full_name',
         'email',
         'image_url',
     ];
 
-    protected array $hidden = [
+    protected $hidden = [
         'remember_token',
     ];
 
-    protected array $casts = [
+    protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
