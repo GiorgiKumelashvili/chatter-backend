40c0330939f83d78ec371a7cbac13f0a3241936e
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2021-03-16 23:00:57 +0400

    some tewaks

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,8 +38,8 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
+                'host' => env('PUSHER_WEBSOCKETS_SERVER', '127.0.0.1'),
+                'port' => env('PUSHER_WEBSOCKETS_PORT', 6001),
                 'scheme' => 'http'
             ],
         ],
