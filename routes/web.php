840af72e1d60b3a692f8fad08af6c585656ce0ae
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2021-03-17 18:44:48 +0400

    another test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/sockets', \BeyondCode\LaravelWebSockets\Dashboard\Http\Controllers\ShowDashboard::class);
